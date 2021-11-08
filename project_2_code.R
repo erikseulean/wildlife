71f2e2b8b8cd249590f87d350f6672be855f4e9c
Erik-Cristian Seulean <erikseulean@gmail.com>
2021-11-08 21:41:05 +0000

    Update BUGS code to include y.est
    Signed-off-by: Erik-Cristian Seulean <erikseulean@gmail.com>

--- project_2_code.R
+++ project_2_code.R
@@ -5,69 +5,73 @@ library(jagsUI)
 library(MCMCvis)
 data("wildebeest")
 wildebeest
-wildebeest = na.locf(wildebeest)
+wildebeest <- na.locf(wildebeest)
 wildebeest
 
-#Prior for beta 0 
-b0 = rnorm(0,0.00001)
-b1 = rnorm(0,0.00001)
-sigma.n = runif(0,1)
-sigma.y = runif(0,1)
+#Prior for beta 0
+# b0 <- rnorm(0, 0.00001)
+# b1 <- rnorm(0, 0.00001)
+# sigma.n <- runif(0, 1)
+# sigma.y <- runif(0, 1)
 
 sink("project2.txt")
 cat("
 model{
 
   # Priors and constraints
-  log.n1 ~ dnorm(0.92333, 0.01)
-  log.N[1] <- log.n1 
-  b0 ~ dnorm(0, 0.00001) #Prior for the Intercept
-  b1 ~ dnorm(0, 0.00001) #Prior for the slope
-  sig.n ~ dunif(0, 1)  #Variance for the state process 
-  sig2.n <- pow(sig.n, 2) #Standard Devation for the state process
-  tau.n <- pow(sig.n, -2) #Precision for the state process
-  sig.y ~ dunif(0, 1) #Variance for the observations
-  sig2.y <- pow(sig.y, 2)
-  tau.y <- pow(sig.y, -2)
+  log.n1 ~ dnorm(0.92333, 0.01) # Initial population size
+  log.N[1] <- log.n1
+    
+  b0 ~ dnorm(0, 0.00001)  # Prior for the Intercept
+  b1 ~ dnorm(0, 0.00001)  # Prior for the slope
+
+  sig.n ~ dunif(0, 1)     # Standard deviation for the population
+  tau.n <- pow(sig.n, -2) # Precision for the state process
+  
+  sig.y ~ dunif(0, 1)     # Standard deviation for the observations process  
+  tau.y <- pow(sig.y, -2) # Precision for the observation process
 
   # Likelihood - State process
-  for(t in 1:(nyrs-1)){
+  for(t in 1:(nyrs-1)) {
     log.N[t+1] ~ dnorm(r[t]+ log.N[t], tau.n)
-    log(r[t]) <- log.r[t] 
+    log(r[t]) <- log.r[t]
     log.r[t] <- b0 + b1*rain[t]
   }
 
   # Likelihood - Observation process
   for (t in 1:nyrs) {
-     y[t] ~ dnorm(log.N[t], tau.y)
-     # y[t] <- exp(log.y[t])
-    
+     log.y[t] ~ dnorm(log.N[t], tau.y)
+
   }
-  
-  # Derive population sizes on real scale
+
+  # Derive population and observation sizes on real scale
   for (t in 1:nyrs) {
     N.est[t] <- exp(log.N[t])
-   
+    y.est[t] <- exp(log.y[t])
   }
 }
-",fill = TRUE)
+", fill = TRUE)
 sink()
 
-projection = 6
+projection <- 6
 
-#passing in parameters from dataset 
-wildebeest_dta <- list(rain = c(wildebeest$rain, rep(NA,projection)), y = c(log(wildebeest$Nhat), rep(NA,projection)) , nyrs = nrow(wildebeest)+projection)
+#passing in parameters from dataset
+wildebeest_dta <- list(
+  rain = c(wildebeest$rain, rep(NA, projection)),
+  y = c(log(wildebeest$Nhat), rep(NA, projection)),
+  nyrs = nrow(wildebeest) + projection
+)
 
 
 # Initial values
-wildebeest_inits <- function(){
-  list(b0 =runif(1,-2,2),
-       b1 = runif(1,-2,2),
+wildebeest_inits <- function() {
+  list(b0 = runif(1, -2, 2),
+       b1 = runif(1, -2, 2),
        sig.y = 1,
-       sig.n = 1 )
+       sig.n = 1)
 }
 
-parms <- c("b0","b1")
+parms <- c("b0", "b1")
 
 nc <- 3
 nb <- 1000
@@ -83,15 +87,9 @@ out <- jags(data = wildebeest_dta,
             n.burnin = nb,
             n.thin = nt)
 out
-par(mar=(c(0,0,10,100)))
+par(mar = (c(0, 0, 10, 100)))
 MCMCtrace(out,                 #the fitted model
-          params = parms, #out parameters of interest
-          iter = ni,                 #plot all iterations
-          pdf = FALSE,               #DON'T write to a PDF
-          ind = FALSE)                #chain specific densities
-
-
-
-
-
-
+          params = parms,      #out parameters of interest
+          iter = ni,           #plot all iterations
+          pdf = FALSE,         #DON'T write to a PDF
+          ind = FALSE)         #chain specific densities
