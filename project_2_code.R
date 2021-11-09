2ca3b00e9f994b20265d7e59c085ddd2fe763cdc
Erik-Cristian Seulean <erikseulean@gmail.com>
2021-11-09 11:46:25 +0000

    Update R code to include stochastic params
    Signed-off-by: Erik-Cristian Seulean <erikseulean@gmail.com>

--- project_2_code.R
+++ project_2_code.R
@@ -8,27 +8,21 @@ wildebeest
 wildebeest <- na.locf(wildebeest)
 wildebeest
 
-#Prior for beta 0
-# b0 <- rnorm(0, 0.00001)
-# b1 <- rnorm(0, 0.00001)
-# sigma.n <- runif(0, 1)
-# sigma.y <- runif(0, 1)
-
 sink("project2.txt")
 cat("
 model{
 
   # Priors and constraints
-  log.n1 ~ dnorm(0.92333, 0.01) # Initial population size
+  log.n1 ~ dnorm(N1, 0.01) # Initial population size
   log.N[1] <- log.n1
-    
+
   b0 ~ dnorm(0, 0.00001)  # Prior for the Intercept
   b1 ~ dnorm(0, 0.00001)  # Prior for the slope
 
   sig.n ~ dunif(0, 1)     # Standard deviation for the population
   tau.n <- pow(sig.n, -2) # Precision for the state process
-  
-  sig.y ~ dunif(0, 1)     # Standard deviation for the observations process  
+
+  sig.y ~ dunif(0, 1)     # Standard deviation for the observations process
   tau.y <- pow(sig.y, -2) # Precision for the observation process
 
   # Likelihood - State process
@@ -55,11 +49,12 @@ sink()
 
 projection <- 6
 
-#passing in parameters from dataset
-wildebeest_dta <- list(
-  rain = c(wildebeest$rain, rep(NA, projection)),
-  y = c(log(wildebeest$Nhat), rep(NA, projection)),
-  nyrs = nrow(wildebeest) + projection
+# Passing in parameters from dataset
+wildebeest_model_parameters <- list(
+  rain = wildebeest$rain, # vector of rain quantity observed each year
+  y = log(wildebeest$Nhat), # vector of population observations
+  nyrs = nrow(wildebeest), # number of years included in the data
+  N1 = log(wildebeest$Nhat[1]) # initial population estimate
 )
 
 
@@ -67,18 +62,19 @@ wildebeest_dta <- list(
 wildebeest_inits <- function() {
   list(b0 = runif(1, -2, 2),
        b1 = runif(1, -2, 2),
-       sig.y = 1,
-       sig.n = 1)
+       sig.n = runif(1, 0, 1),
+       sig.y = runif(1, 0, 1)
+  )
 }
 
-parms <- c("b0", "b1")
+parms <- c("b0", "b1", "N.est", "y.est")
 
 nc <- 3
-nb <- 1000
-ni <- 5000 + nb
+nb <- 100000
+ni <- 100000 + nb
 nt <- 1
 
-out <- jags(data = wildebeest_dta,
+out <- jags(data = wildebeest_model_parameters,
             inits = wildebeest_inits,
             parameters.to.save = parms,
             model.file = "project2.txt",
@@ -86,10 +82,8 @@ out <- jags(data = wildebeest_dta,
             n.iter = ni,
             n.burnin = nb,
             n.thin = nt)
-out
-par(mar = (c(0, 0, 10, 100)))
-MCMCtrace(out,                 #the fitted model
-          params = parms,      #out parameters of interest
-          iter = ni,           #plot all iterations
-          pdf = FALSE,         #DON'T write to a PDF
-          ind = FALSE)         #chain specific densities
+MCMCtrace(out,                      #the fitted model
+          params = parms[1:2],      #out parameters of interest
+          iter = ni,                #plot all iterations
+          pdf = FALSE,              #DON'T write to a PDF
+          ind = FALSE)              #chain specific densities
